562a216287d5a5c1aa6d35a4f457742542ec2348
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-08 14:25:03 +0100

    List categories w/ include products (/api/categories_includes)

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -9,4 +9,14 @@ router.get('/categories', async (_req, res) => {
   res.json(categories)
 })
 
+router.get('/categories_include', async (_req, res) => {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: true
+    }
+  })
+
+  res.json(categories)
+})
+
 export default router
